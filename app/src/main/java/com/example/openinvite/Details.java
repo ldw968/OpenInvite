179facd6aefb517aeb335c2e0862b5f596bba9ef
jdrake5 <jdrake8@gmail.com>
2018-09-15 18:58:07 -0400

    hi

--- app/src/main/java/com/example/openinvite/Details.java
+++ app/src/main/java/com/example/openinvite/Details.java
@@ -1,13 +1,34 @@
 package com.example.openinvite;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+//import com.squareup.picasso.Picasso;
 
 public class Details extends AppCompatActivity {
+
+    private TextView titleTextView;
+    private TextView releaseDateView;
+    private TextView descriptionView;
+    private ImageView posterView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        // get references to the views.
+        titleTextView = (TextView) findViewById(R.id.title);
+        releaseDateView = (TextView) findViewById(R.id.descr);
+        posterView = (ImageView) findViewById(R.id.imageView);
+
+        Intent intent = getIntent();
+        Event event = intent.getParcelableExtra(EventsAdapter.EVENT);
+
+        titleTextView.setText(event.get_name());
+        releaseDateView.setText("Release Date: " + event.get_descr());
+        //Picasso.with(posterView.getContext()).load(movie.getPoster_path()).into(posterView);
     }
 }
